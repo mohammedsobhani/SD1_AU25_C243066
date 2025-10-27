e7eee6ba1b3a452a2fe99b48f423133aaff4dc59
Syed Mohammed Sobhani <165961904+mohammedsobhani@users.noreply.github.com>
2025-10-27 09:05:50 +0600

    Implement calculateMinimumCost method
    Added a method to calculate the minimum cost for subscriptions based on the number of friends and subscription cost.

--- IIUCTV/src/iiuctv/IIUCTV.java
+++ IIUCTV/src/iiuctv/IIUCTV.java
@@ -5,6 +5,8 @@
  */
 package iiuctv;
 
+import java.util.Scanner;
+
 /**
  *
  * @author moham
@@ -14,8 +16,22 @@ public class IIUCTV {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static int calculateMinimumCost(int N, int X) {
+        // Each subscription can be shared by 6 people
+        int subscriptionsNeeded = (N + 5) / 6;  // Round up division
+        return subscriptionsNeeded * X;
     }
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of friends (N): ");
+        int N = scanner.nextInt();
+
+        System.out.print("Enter cost per subscription (X): ");
+        int X = scanner.nextInt();
+
+        int totalCost = calculateMinimumCost(N, X);
+        System.out.println("Minimum total cost: " + totalCost + " Taka");
+    }
 }
